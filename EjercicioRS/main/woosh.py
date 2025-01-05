c8647610ad0f39e517c66b729ba3ec51758d8c1b
franciiscocg <cisquitofrancisco@gmail.com>
2025-01-05 19:32:59 +0100

    algunas consultas woosh

--- EjercicioRS/main/woosh.py
+++ EjercicioRS/main/woosh.py
@@ -7,6 +7,7 @@ from whoosh.qparser import QueryParser, MultifieldParser
 from whoosh import qparser, query
 from beautifulSoup import almacenar_bd
 
+index_dir = os.path.join("EjercicioRS", "Index")
 #Crea un indice desde los documentos contenidos en dirdocs
 #El indice lo crea en el directorio dirindex 
 def crea_index():
@@ -16,14 +17,14 @@ def crea_index():
                    genero=KEYWORD(stored=True, commas=True), 
                    date=DATETIME(stored=True), 
                    idioma=KEYWORD(stored=True, commas=True))
-    
+
     #eliminamos el directorio del Ã­ndice, si existe
-    if os.path.exists("Index"):
-        shutil.rmtree("Index")
-    os.mkdir("Index")
+    if os.path.exists(index_dir):
+        shutil.rmtree(index_dir)
+    os.mkdir(index_dir)
     
     #creamos el Ã­ndice
-    ix = create_in("Index", schema=schem)
+    ix = create_in(index_dir, schema=schem)
     #creamos un writer para poder aÃ±adir documentos al indice
     writer = ix.writer()
     i=0
@@ -34,154 +35,49 @@ def crea_index():
         i+=1
     writer.commit()  
 
+    
+def titulo_o_descripcion(palabra):
+    ix = open_dir(index_dir)
+    with ix.searcher() as searcher:
+        query = MultifieldParser(["titulo", "descripcion"], ix.schema).parse(palabra)
+        results = searcher.search(query)
+        result_list = []
+        for result in results:
+            result_list.append(result)
+        return result_list
+
+def genero(palabra):
+    ix = open_dir(index_dir)
+    with ix.searcher() as searcher:
+        query = QueryParser("genero", ix.schema).parse(palabra)
+        results = searcher.search(query)
+        result_list = []
+        for result in results:
+            result_list.append(result)
+        return result_list
+    
+def nota(nota):
+    ix = open_dir(index_dir)
+    with ix.searcher() as searcher:
+        query = QueryParser("nota", ix.schema).parse(nota)
+        results = searcher.search(query)
+        result_list = []
+        for result in results:
+            result_list.append(result)
+        return result_list
+
+def idioma(palabra):
+    ix = open_dir(index_dir)
+    with ix.searcher() as searcher:
+        query = QueryParser("idioma", ix.schema).parse(palabra)
+        results = searcher.search(query)
+        result_list = []
+        for result in results:
+            result_list.append(result)
+        return result_list 
         
-        
-def asunto_o_cuerpo():
-    def listar_asunto_o_cuerpo(event):
-            ix=open_dir(dirindex)   
-            with ix.searcher() as searcher:
-                myquery = MultifieldParser(["asunto","contenido"], ix.schema).parse(str(entry.get()))
-                results = searcher.search(myquery)
-                listar(results)
-            
-    v = Toplevel()
-    label = Label(v, text="Introduzca consulta sobre asunto o contenido: ")
-    label.pack(side=LEFT)
-    entry = Entry(v)
-    entry.bind("<Return>", listar_asunto_o_cuerpo)
-    entry.pack(side=LEFT)
     
         
-def posteriores_a_fecha():  
-    def listar_fecha(event):
-            myquery='{'+ str(entry.get()) + ' TO]'
-            ix=open_dir(dirindex)   
-            try:
-                with ix.searcher() as searcher:
-                    query = QueryParser("fecha", ix.schema).parse(myquery)
-                    results = searcher.search(query)
-                    listar(results)
-            except:
-                messagebox.showerror("ERROR", "Formato de fecha incorrecto")
-            
-    v = Toplevel()
-    label = Label(v, text="Introduzca la fecha (AAAAMMDD): ")
-    label.pack(side=LEFT)
-    entry = Entry(v)
-    entry.bind("<Return>", listar_fecha)
-    entry.pack(side=LEFT)
-              
-
-def spam():
-    def listar_spam(event):
-        ix=open_dir(dirindex)   
-        with ix.searcher() as searcher:
-            query = QueryParser("asunto", ix.schema,group=qparser.OrGroup).parse(str(entry.get()))
-            results = searcher.search(query)
-            
-            v1 = Toplevel()
-            sc = Scrollbar(v1)
-            sc.pack(side=RIGHT, fill=Y)
-            lb = Listbox(v1, width=100, yscrollcommand=sc.set)
-            for row in results:
-                s = 'FICHERO: ' + row['nombrefichero']
-                lb.insert(END, s)
-                s = 'REMITENTE: ' + agenda[row['remitente']]
-                lb.insert(END, s)
-                lb.insert(END, "-------------------------------")       
-            lb.pack(side=LEFT, fill=BOTH)
-            sc.config(command=lb.yview)        
-    v = Toplevel()
-    label = Label(v, text="Introduzca palabras spam: ")
-    label.pack(side=LEFT)
-    entry = Entry(v)
-    entry.bind("<Return>", listar_spam)
-    entry.pack(side=LEFT)           
-
-
-def add_doc(writer, path, docname):
-    try:
-        fileobj=open(path+'\\'+docname, "r")
-        rte=fileobj.readline().strip()
-        dtos=fileobj.readline().strip()
-        f=fileobj.readline().strip()
-        dat=datetime.strptime(f,'%Y%m%d')
-        ast=fileobj.readline().strip()
-        ctdo=fileobj.read()
-        fileobj.close()           
-        
-        writer.add_document(remitente=rte, destinatarios=dtos, fecha=dat, asunto=ast, contenido=ctdo, nombrefichero=docname)
-    
-    except:
-        messagebox.showerror("ERROR", "Error: No se ha podido aÃ±adir el documento "+path+'\\'+docname)
-
-
-def crea_agenda():
-    try:
-        fileobj=open(dirage+'\\'+"agenda.txt", "r")
-        email=fileobj.readline()
-        while email:
-            nombre=fileobj.readline()
-            agenda[email.strip()]=nombre.strip()
-            email=fileobj.readline()
-    except:
-        messagebox.showerror("ERROR", "No se ha podido crear la agenda. Compruebe que existe el fichero "+dirage+'\\'+"agenda.txt")
-           
-
-def cargar():
-    crea_index()
-    crea_agenda()
-    
-def listar(results):      
-    v = Toplevel()
-    sc = Scrollbar(v)
-    sc.pack(side=RIGHT, fill=Y)
-    lb = Listbox(v, width=150, yscrollcommand=sc.set)
-    for row in results:
-        s = 'REMITENTE: ' + row['remitente']
-        lb.insert(END, s)       
-        s = "DESTINATARIOS: " + row['destinatarios']
-        lb.insert(END, s)
-        s = "FECHA: " + row['fecha'].strftime('%d-%m-%Y')
-        lb.insert(END, s)
-        s = "ASUNTO: " + row['asunto']
-        lb.insert(END, s)
-        s = "CUERPO: " + row['contenido']
-        lb.insert(END, s)
-        lb.insert(END,"------------------------------------------------------------------------\n")
-    lb.pack(side=LEFT, fill=BOTH)
-    sc.config(command=lb.yview)
-
-
-def ventana_principal():
-    def listar_todo():
-        ix=open_dir(dirindex)
-        with ix.searcher() as searcher:
-            results = searcher.search(query.Every(),limit=None)
-            listar(results) 
-    
-    raiz = Tk()
-
-    menu = Menu(raiz)
-
-    #DATOS
-    menudatos = Menu(menu, tearoff=0)
-    menudatos.add_command(label="Cargar", command=cargar)
-    menudatos.add_command(label="Listar", command=listar_todo)
-    menudatos.add_command(label="Salir", command=raiz.quit)
-    menu.add_cascade(label="Datos", menu=menudatos)
-
-    #BUSCAR
-    menubuscar = Menu(menu, tearoff=0)
-    menubuscar.add_command(label="Asunto o Cuerpo", command=asunto_o_cuerpo)
-    menubuscar.add_command(label="Posteriores a una Fecha", command=posteriores_a_fecha)
-    menubuscar.add_command(label="Spam", command=spam)
-    menu.add_cascade(label="Buscar", menu=menubuscar)
-
-    raiz.config(menu=menu)
-
-    raiz.mainloop()
-
 
 if __name__ == "__main__":
     crea_index()
